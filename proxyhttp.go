419cc82a2db1d7162c6ed1f3f6d316cb0def8daa
markus <markus.karileet@codehouse.ee>
2015-10-28 23:34:51 +0200

    implemented global counter

--- proxyhttp.go
+++ proxyhttp.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"log"
 	"flag"
+	"sync/atomic"
 )
 
 var fromHost = flag.String("f", ":12345", "The proxy server's port")
@@ -13,7 +14,10 @@ var toHost = flag.String("t", "http://wsf.cdyne.com/WeatherWS/Weather.asmx", "Th
 var maxConnections = flag.Int("c", 25, "The maximum number of concurrent connection")
 var needle = flag.String("n", "94301", "The needle to search from the requests")
 
+var matches uint64 = 0
+
 const POST_TYPE = "text/xml;charset=UTF-8"
+const MATCH_FOUND_STR = "Match found! #"
 
 /**
  * App listens on TCP 12345. everything posted on this port will be submitted to cdyne weather webservice
@@ -21,31 +25,20 @@ const POST_TYPE = "text/xml;charset=UTF-8"
 func ProxyServer(w http.ResponseWriter, request *http.Request) {
 	responseChannel := make(chan []byte, *maxConnections)
 	defer close(responseChannel)
-	go worker(request, responseChannel)
-	w.Write(<-responseChannel)
-}
-
-func worker(request *http.Request, responseChannel chan []byte) {
-	requestBody, _ := ioutil.ReadAll(request.Body)
-	request.Body.Close()
-	if strings.Contains(string(requestBody), *needle) {
-		log.Println("Contains match!")
-	}
-	response, _:= http.Post(*toHost, POST_TYPE, strings.NewReader(string(requestBody)))//TODO: bytes.NewBuffer(rbytes[:read]))
-	responseBody, _ := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-	responseChannel <- responseBody
-}
-
-func generator () {
-	id := make(chan int64)
 	go func() {
-		var counter int64 = 0
-		for {
-			id <- counter
-			counter += 1
+		requestBody, _ := ioutil.ReadAll(request.Body)
+		request.Body.Close()
+		if strings.Contains(string(requestBody), *needle) {
+			atomic.AddUint64(&matches, 1)
+			log.Println(MATCH_FOUND_STR, atomic.LoadUint64(&matches))
 		}
+		response, _:= http.Post(*toHost, POST_TYPE, strings.NewReader(string(requestBody)))//TODO: bytes.NewBuffer(rbytes[:read]))
+		responseBody, _ := ioutil.ReadAll(response.Body)
+		response.Body.Close()
+		responseChannel <- responseBody
 	}()
+
+	w.Write(<-responseChannel)
 }
 
 func main() {
